dfffb6d876a6f588e1c1b488a54bcfd1e6ba4a4c
Gleb Staheev <g.staheev@yandex.ru>
2024-09-02 16:57:15 +0300

    add filter into search

--- src/main/java/com/social/mc_post/services/impl/PostServiceImpl.java
+++ src/main/java/com/social/mc_post/services/impl/PostServiceImpl.java
@@ -89,6 +89,7 @@ public class PostServiceImpl implements PostService {
         PostSearchDto finalSearchDto = searchDto;
         List<PostDto> posts = postRepository.findAll(spec, PageRequest.of(pageDto.getPage(), pageDto.getSize())).stream()
                 .filter(post -> finalSearchDto.getAccountIds().contains(post.getAuthorId()))
+                .filter(post -> post.getType().equals(TypePost.POSTED))
                 .map(postMapper::mapEntityToDto)
                 .toList();
         Sort sort = Sort.unsorted();
