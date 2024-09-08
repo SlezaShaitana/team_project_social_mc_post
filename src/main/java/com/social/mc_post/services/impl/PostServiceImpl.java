915392fd8a8acea3f244802acd5817ae1f011d1e
Gleb Staheev <g.staheev@yandex.ru>
2024-09-08 07:06:45 +0300

    change post search 4

--- src/main/java/com/social/mc_post/services/impl/PostServiceImpl.java
+++ src/main/java/com/social/mc_post/services/impl/PostServiceImpl.java
@@ -344,6 +344,10 @@ public class PostServiceImpl implements PostService {
                     data[0],
                     data[1],
                     size).getContent();
+            accounts.addAll(accountClient.getListAccounts(headerRequestByAuth,
+                    data[1],
+                    data[0],
+                    size).getContent());
             log.info(accounts.toString());
             return accounts.stream()
                     .map(AccountMeDTO::getId)
