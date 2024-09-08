f94f9f2e8b956c5d3cd45b5bf95a02e54bf17810
Gleb Staheev <g.staheev@yandex.ru>
2024-09-08 14:22:26 +0300

    change post search 9

--- src/main/java/com/social/mc_post/feign/AccountClient.java
+++ src/main/java/com/social/mc_post/feign/AccountClient.java
@@ -1,10 +1,7 @@
 package com.social.mc_post.feign;
 
-import com.social.mc_post.dto.account.AccountMeDTO;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
